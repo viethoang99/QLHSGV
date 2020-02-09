ed9164692f0f8932df1f7f8e4537a3dbb5485a9a
theking <38319601+thg6060@users.noreply.github.com>
2020-02-09 18:58:13 +0700

    Giang làm về giáo viên

--- QLHocSinhTHPT/app/DataLayer/DiemData.cs
+++ QLHocSinhTHPT/app/DataLayer/DiemData.cs
@@ -16,11 +16,33 @@ namespace app.DataLayer
         public DataTable LayDsDiem(String maHocSinh, String maMonHoc, String maHocKy, String maNamHoc, String maLop)
         {
 
+            SqlCommand cmd = new SqlCommand("select * from LOAIDIEM ld ,DIEM d , HOCSINH hs , MONHOC mh where d.MaHocSinh = @maHocSinh " +
+                 "and d.MaMonHoc = @maMonHoc and d.MaHocKy = @maHocKy and d.MaNamHoc = @maNamHoc and d.MaLop = @maLop " +
+                 "	and d.MaHocSinh = hs.MaHocSinh and mh.MaMonHoc = d.MaMonHoc and ld.MaLoai = d.MaLoai ");
+            cmd.Parameters.Add("maHocSinh", SqlDbType.VarChar).Value = maHocSinh;
+            cmd.Parameters.Add("maMonHoc", SqlDbType.VarChar).Value = maMonHoc;
+            cmd.Parameters.Add("maHocKy", SqlDbType.VarChar).Value = maHocKy;
+            cmd.Parameters.Add("maNamHoc", SqlDbType.VarChar).Value = maNamHoc;
+            cmd.Parameters.Add("maLop", SqlDbType.VarChar).Value = maLop;
+
+            m_DiemData.Load(cmd);
             return m_DiemData;
         }
 
         public void LuuDiem(String maHocSinh, String maMonHoc, String maHocKy, String maNamHoc, String maLop, String maLoaiDiem, float diemSo)
         {
+            SqlCommand cmd = new SqlCommand("INSERT DIEM(MaHocSinh,MaMonHoc,MaHocKy,MaNamHoc,MaLop,MaLoai,Diem)" +
+              " VALUES(@maHocSinh,@maMonHoc,@maHocKy,@maNamHoc,@maLop,@maLoaiDiem,@diemSo) ");
+            cmd.Parameters.Add("maHocSinh", SqlDbType.VarChar).Value = maHocSinh;
+            cmd.Parameters.Add("maMonHoc", SqlDbType.VarChar).Value = maMonHoc;
+            cmd.Parameters.Add("maHocKy", SqlDbType.VarChar).Value = maHocKy;
+            cmd.Parameters.Add("maNamHoc", SqlDbType.VarChar).Value = maNamHoc;
+            cmd.Parameters.Add("maLop", SqlDbType.VarChar).Value = maLop;
+            cmd.Parameters.Add("maLoaiDiem", SqlDbType.VarChar).Value = maLoaiDiem;
+            cmd.Parameters.Add("diemSo", SqlDbType.Float).Value = diemSo;
+
+
+            m_DiemData.Load(cmd);
         }
 
         public void XoaDiem(int stt)
--- QLHocSinhTHPT/app/DataLayer/LopData.cs
+++ QLHocSinhTHPT/app/DataLayer/LopData.cs
@@ -17,6 +17,13 @@ namespace app.DataLayer
 
         public DataTable LayDsLop(String namHoc)
         {
+
+            SqlCommand cmd = new SqlCommand("SELECT l.MaLop,l.TenLop,l.MaKhoiLop,l.MaNamHoc,l.SiSo,l.MaGiaoVien" +
+                " FROM LOP as l ,NAMHOC as nh" +
+                " where l.MaNamHoc = nh.MaNamHoc and nh.MaNamHoc = @namHoc ");
+            cmd.Parameters.Add("namHoc", SqlDbType.VarChar).Value = namHoc;
+            m_LopData.Load(cmd);
+            return m_LopData;
             return m_LopData;
         }
 
