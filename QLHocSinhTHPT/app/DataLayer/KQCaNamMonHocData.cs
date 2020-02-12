9fd9c86bde5e3099d6106442c02c8c212d75c3a2
viethoang99 <viet.mta.hp@gmail.com>
2020-02-12 23:41:31 +0700

    thong ke

---
+++ QLHocSinhTHPT/app/DataLayer/KQCaNamMonHocData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace app.DataLayer
+{
+    public class KQCaNamMonHocData
+    {
+        DataService m_KQCaNamMonHocData = new DataService();
+
+        public void LuuKetQua(String maHocSinh, String maLop, String maMonHoc, String maNamHoc, float diemThiLai, float diemTBMonCN)
+        {
+
+        }
+
+        public void XoaKetQua(String maHocSinh, String maLop, String maMonHoc, String maNamHoc)
+        {
+            
+        }
+
+        public DataTable LayDsKQCaNamMonHocForReport(String maLop, String maMonHoc, String maNamHoc)
+        {
+           
+            return m_KQCaNamMonHocData;
+        }
+    }
+}
---
+++ QLHocSinhTHPT/app/DataLayer/KQHocKyMonHocData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace app.DataLayer
+{
+    public class KQHocKyMonHocData
+    {
+        DataService m_KQHocKyMonHocData = new DataService();
+
+        public void LuuKetQua(String maHocSinh, String maLop, String maMonHoc, String maHocKy, String maNamHoc, float diemTBKT, float diemTBMonHK)
+        {
+        }
+
+        public void XoaKetQua(String maHocSinh, String maLop, String maMonHoc, String maHocKy, String maNamHoc)
+        {
+            
+        }
+
+        public DataTable LayDsKQHocKyMonHocForReport(String maLop, String maMonHoc, String maHocKy, String maNamHoc)
+        {
+            return m_KQHocKyMonHocData;
+        }
+    }
+}
